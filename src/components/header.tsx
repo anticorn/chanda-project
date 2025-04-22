e02be0dcce7bc59b8b9ad522acd8b476c1a0cc4a
anticorn <anticorn@gmail.com>
2025-04-22 11:47:36 -0400

    trying out some changes to the header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,10 +2,19 @@ import { Link } from 'waku';
 
 export const Header = () => {
   return (
-    <header className="flex items-center gap-4 p-6 lg:fixed lg:left-0 lg:top-0">
+    <header className="flex flex-col gap-4 p-6 lg:fixed lg:left-0 lg:top-0">
+
       <h2 className="text-lg font-bold tracking-tight">
-        <Link to="/">Waku starter</Link>
+        <Link to="/">Chandas landing page</Link>
       </h2>
+      <h2>Welcome to Rose&Stone Bookkeeping</h2>
+      <figure>
+        <img
+          src="/images/bookeeping.png"
+          alt="a book being kept"
+        />
+        <figcaption>Rose&Stone</figcaption>
+      </figure>
     </header>
   );
 };
