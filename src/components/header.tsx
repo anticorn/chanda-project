dd65867e0349cde4efcdeccb43516cde46769405
anticorn <anticorn@gmail.com>
2025-04-24 22:25:12 -0400

    some positioning of header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,16 +2,17 @@ import { Link } from 'waku';
 
 export const Header = () => {
   return (
-    <header className="flex flex-col gap-4 p-6 lg:fixed lg:left-0 lg:top-0">
+    <header className="flex flex-col gap-4 p-6 lg:fixed lg:inset-0 lg:m-auto lg:w-full lg:max-w-4xl">
 
       <h2 className="text-lg font-bold tracking-tight">
         <Link to="/">Chandas landing page</Link>
       </h2>
       <h2>Welcome to Rose&Stone Bookkeeping</h2>
-      <figure>
+      <figure className="w-full max-w-md">
         <img
           src="/images/bookeeping.png"
           alt="a book being kept"
+          className="max-w-full h-auto"
         />
         <figcaption>Rose&Stone</figcaption>
       </figure>
